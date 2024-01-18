fa5b7609893fb3c2b0271ec9b89c1743777a69e6
orgmatter <orgmatter@gmail.com>
2024-01-18 18:13:56 +0100

    modification of login action file

--- src/store/actions/auth/login-action.js
+++ src/store/actions/auth/login-action.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTypes } from "../../../hooks/useTypes";
 import useAuth from "../../../hooks/useAuth";
+import { authDummy as AUTH_DUMMY } from "../../dummy-data/auth-dummy";
 
 export const LoginAction = (params) => {
 
@@ -21,24 +22,16 @@ export const LoginAction = (params) => {
         dispatch({
             type: LOGIN_START
         })
-    
-        fetch("https://8cdb4d4ccf7b4659975655ecf5dc7820.api.mockbin.io/", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(resp => resp.json())
-        .then(res => {
-            const admin = res.admin.find(admin => admin.email === email && admin.password === password);
+
+        try {
+            const admin = AUTH_DUMMY.find(auth => auth.email === email && auth.password === password);
             if(Object.keys(admin).length > 0) {
                 login(admin);
                 dispatch({
                     type: LOGIN_SUCCESS
                 })
             }
-        })
-        .catch(e => {
+        }catch(e) {
             Object.keys(e).length === 0 ?
             dispatch({
                 type: LOGIN_CREDENTIALS_FAILED,
@@ -48,6 +41,6 @@ export const LoginAction = (params) => {
                 type: LOGIN_FAILED,
                 msg: ""
             })
-        })
+        }
     }
 }
---
+++ src/store/dummy-data/auth-dummy.js
@@ -0,0 +1,20 @@
+export const authDummy = [
+    {
+        email: "admin1@lendsqr.com",
+        password: "password1",
+        firstname: "Adedeji",
+        lastname: "Ojo"
+    },
+    {
+        email: "admin2@lendsqr.com",
+        password: "password2",
+        firstname: "Yinka",
+        lastname: "Quadiri"
+    },
+    {
+        email: "admin3@lendsqr.com",
+        password: "password3",
+        firstname: "Funke",
+        lastname: "Ajayi"
+    }
+]
